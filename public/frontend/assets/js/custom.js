eebefb032e98236d0d993180e95f2583d17e7a74
Kpokharel3 <krishna.pokharel@elscript.com>
2022-08-28 19:59:00 +0545

    product filter and filter sort changed to option and select

--- public/frontend/assets/js/custom.js
+++ public/frontend/assets/js/custom.js
@@ -6,7 +6,7 @@ $(".color-circle").on("click", function () {
 
 $(".sub-menu-click").hover(function () {
     var category = $(this).attr("data-category");
-    $('.pane').css("display", "none");
+    $(".pane").css("display", "none");
     $("." + category).css("display", "block");
 });
 
@@ -19,178 +19,92 @@ $(".pane").hover(
 
 reply = () => {
     let replyInput = document.getElementById("replyComment");
-    replyInput.style.display ="block";
-}
+    replyInput.style.display = "block";
+};
 // Custom dropdown
-$(function() {
-    var dd1 = new dropDown($('#myDropdown'));
-    
-    $(document).click(function() {
-      $('.wrapper-dropdown').removeClass('active');
-    });
-  });
-  
-  function dropDown(el) {
-    this.dd = el;
-    this.placeholder = this.dd.children('span');
-    this.opts = this.dd.find('ul.dropdown > li');
-    this.val = '';
-    this.index = -1;
-    this.initEvents();
-  }
-  dropDown.prototype = {
-    initEvents: function() {
-      var obj = this;
-      
-      obj.dd.on('click', function() {
-        $(this).toggleClass('active');
-        return false;
-      });
-      
-      obj.opts.on('click', function() {
-        var opt = $(this);
-        obj.val = opt.text();
-        obj.index = opt.index();
-        obj.placeholder.text(obj.val);
-      });
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /*for each element, create a new DIV that will act as the selected item:*/
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /*for each element, create a new DIV that will contain the option list:*/
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+        /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function (e) {
+            /*when an item is clicked, update the original select box,
+        and the selected item:*/
+            var y, i, k, s, h, sl, yl;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            sl = s.length;
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < sl; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y =
+                        this.parentNode.getElementsByClassName(
+                            "same-as-selected"
+                        );
+                    yl = y.length;
+                    for (k = 0; k < yl; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            h.click();
+        });
+        b.appendChild(c);
     }
-  }
-//   Filter item dropdown
-// Price
-$(function() {
-    var dd1 = new dropDown($('#myDropdown1'));
-    
-    $(document).click(function() {
-      $('.wrapper-dropdown').removeClass('active');
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+        /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
     });
-  });
-  
-  function dropDown(el) {
-    this.dd = el;
-    this.placeholder = this.dd.children('span');
-    this.opts = this.dd.find('ul.dropdown > li');
-    this.val = '';
-    this.index = -1;
-    this.initEvents();
-  }
-  dropDown.prototype = {
-    initEvents: function() {
-      var obj = this;
-      
-      obj.dd.on('click', function() {
-        $(this).toggleClass('active');
-        return false;
-      });
-      
-      obj.opts.on('click', function() {
-        var opt = $(this);
-        obj.val = opt.text();
-        obj.index = opt.index();
-        obj.placeholder.text(obj.val);
-      });
+}
+function closeAllSelect(elmnt) {
+    /*a function that will close all select boxes in the document,
+  except the current select box:*/
+    var x,
+        y,
+        i,
+        xl,
+        yl,
+        arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+            arrNo.push(i);
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
     }
-  }
-
-//   size
-$(function() {
-    var dd1 = new dropDown($('#myDropdown2'));
-    
-    $(document).click(function() {
-      $('.wrapper-dropdown').removeClass('active');
-    });
-  });
-  
-  function dropDown(el) {
-    this.dd = el;
-    this.placeholder = this.dd.children('span');
-    this.opts = this.dd.find('ul.dropdown > li');
-    this.val = '';
-    this.index = -1;
-    this.initEvents();
-  }
-  dropDown.prototype = {
-    initEvents: function() {
-      var obj = this;
-      
-      obj.dd.on('click', function() {
-        $(this).toggleClass('active');
-        return false;
-      });
-      
-      obj.opts.on('click', function() {
-        var opt = $(this);
-        obj.val = opt.text();
-        obj.index = opt.index();
-        obj.placeholder.text(obj.val);
-      });
+    for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
     }
-  }
-
-// color
-$(function() {
-    var dd1 = new dropDown($('#myDropdown3'));
-    
-    $(document).click(function() {
-      $('.wrapper-dropdown').removeClass('active');
-    });
-  });
-  
-  function dropDown(el) {
-    this.dd = el;
-    this.placeholder = this.dd.children('span');
-    this.opts = this.dd.find('ul.dropdown > li');
-    this.val = '';
-    this.index = -1;
-    this.initEvents();
-  }
-  dropDown.prototype = {
-    initEvents: function() {
-      var obj = this;
-      
-      obj.dd.on('click', function() {
-        $(this).toggleClass('active');
-        return false;
-      });
-      
-      obj.opts.on('click', function() {
-        var opt = $(this);
-        obj.val = opt.text();
-        obj.index = opt.index();
-        obj.placeholder.text(obj.val);
-      });
-    }
-  }
-// product
-$(function() {
-    var dd1 = new dropDown($('#myDropdown4'));
-    
-    $(document).click(function() {
-      $('.wrapper-dropdown').removeClass('active');
-    });
-  });
-  
-  function dropDown(el) {
-    this.dd = el;
-    this.placeholder = this.dd.children('span');
-    this.opts = this.dd.find('ul.dropdown > li');
-    this.val = '';
-    this.index = -1;
-    this.initEvents();
-  }
-  dropDown.prototype = {
-    initEvents: function() {
-      var obj = this;
-      
-      obj.dd.on('click', function() {
-        $(this).toggleClass('active');
-        return false;
-      });
-      
-      obj.opts.on('click', function() {
-        var opt = $(this);
-        obj.val = opt.text();
-        obj.index = opt.index();
-        obj.placeholder.text(obj.val);
-      });
-    }
-  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
