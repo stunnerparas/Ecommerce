33cf0ccaf116cb1f455df6b2b3a0e1d95bbdfb8f
Kpokharel3 <krishna.pokharel@elscript.com>
2022-08-29 14:14:29 +0545

    unwanted comment removed

--- public/frontend/assets/js/custom.js
+++ public/frontend/assets/js/custom.js
@@ -23,28 +23,26 @@ reply = () => {
 };
 // Custom dropdown
 var x, i, j, l, ll, selElmnt, a, b, c;
-/*look for any elements with the class "custom-select":*/
+
 x = document.getElementsByClassName("custom-select");
 l = x.length;
 for (i = 0; i < l; i++) {
     selElmnt = x[i].getElementsByTagName("select")[0];
     ll = selElmnt.length;
-    /*for each element, create a new DIV that will act as the selected item:*/
+    
     a = document.createElement("DIV");
     a.setAttribute("class", "select-selected");
     a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
     x[i].appendChild(a);
-    /*for each element, create a new DIV that will contain the option list:*/
+
     b = document.createElement("DIV");
     b.setAttribute("class", "select-items select-hide");
     for (j = 1; j < ll; j++) {
-        /*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
+     
         c = document.createElement("DIV");
         c.innerHTML = selElmnt.options[j].innerHTML;
         c.addEventListener("click", function (e) {
-            /*when an item is clicked, update the original select box,
-        and the selected item:*/
+           
             var y, i, k, s, h, sl, yl;
             s = this.parentNode.parentNode.getElementsByTagName("select")[0];
             sl = s.length;
@@ -71,8 +69,7 @@ for (i = 0; i < l; i++) {
     }
     x[i].appendChild(b);
     a.addEventListener("click", function (e) {
-        /*when the select box is clicked, close any other select boxes,
-      and open/close the current select box:*/
+       
         e.stopPropagation();
         closeAllSelect(this);
         this.nextSibling.classList.toggle("select-hide");
@@ -80,8 +77,7 @@ for (i = 0; i < l; i++) {
     });
 }
 function closeAllSelect(elmnt) {
-    /*a function that will close all select boxes in the document,
-  except the current select box:*/
+   
     var x,
         y,
         i,
@@ -105,6 +101,5 @@ function closeAllSelect(elmnt) {
         }
     }
 }
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
+
 document.addEventListener("click", closeAllSelect);
