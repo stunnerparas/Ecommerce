62a57f06a185cd86594231f43975f0612f31cd86
Krishna Pokharel <102601703+elscriptKrishnaa@users.noreply.github.com>
2022-10-19 13:16:50 +0545

    Removed google icon function

--- public/frontend/assets/js/script.js
+++ public/frontend/assets/js/script.js
@@ -307,10 +307,7 @@
         $(".product-image1").scroll();
     });
 })(jQuery);
-// Google icon loads only after certain time
-$(window).load(function() {
-    $('.google-icon').css('opacity','1');
-});
+
 (function () {
     "use strict";
     var jQueryPlugin = (window.jQueryPlugin = function (ident, func) {
