386673b6882d758557b6ef01b7ca9ad2355e4a5f
Krishna Pokharel <102601703+elscriptKrishnaa@users.noreply.github.com>
2022-10-19 13:36:27 +0545

    loading icon added

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -35,6 +35,7 @@
 </head>
 
 <body>
+<div class="se-pre-con"></div>
 
     <div class="super-container">
         @include('frontend.layouts.nav')
@@ -54,6 +55,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
