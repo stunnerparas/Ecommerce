c7846e40a7f0e726de545924f82509e53ef3274b
Kpokharel3 <krishna.pokharel@elscript.com>
2022-11-08 19:53:35 +0545

    track order page completed and faqs and help desk page linked

--- resources/views/frontend/pages/faq.blade.php
+++ resources/views/frontend/pages/faq.blade.php
@@ -1,32 +1,34 @@
 @extends('frontend.layouts.master')
 
 @section('content')
-    <div class="container-fluid my-5">
-        <div class="container">
-            <h4 class="text-center">Frequently Asked Question</h4>
-            <div class="faq-wrapper">
-                <div class="row">
-                    <div class="col-md-6 col-12 order-1 order-md-1">
-                        <div class="faq-container mt-5">
-                            @foreach ($faqs as $faq)
-                                <div class="faq">
-                                    <h3 class="faq-title">
-                                        {{ $faq->title ?? '' }}
-                                    </h3>
-
-                                    <p class="faq-text text-justify">
-                                        {{ strip_tags($faq->description ?? '') }}
-                                    </p>
-
-                                    <button class="faq-toggle">
-                                        <i class="bi bi-chevron-down"></i>
-                                        <i class="bi bi-x"></i>
-                                    </button>
-                                </div>
-                            @endforeach
+<div class="container-fluid my-5">
+    <div class="container">
+        <h4 class="text-center">Frequently Asked Question</h4>
+        <div class="faq-wrapper">
+            <div class="row">
+                <div class="col-md-6 col-12 order-1 order-md-1">
+                    <div class="faq-container mt-5">
 
 
-                            {{-- <div class="faq">
+                        @foreach ($faqs as $faq)
+                        <div class="faq">
+                            <h3 class="faq-title">
+                                {{ $faq->title ?? '' }}
+                            </h3>
+
+                            <p class="faq-text text-justify">
+                                {{ strip_tags($faq->description ?? '') }}
+                            </p>
+
+                            <button class="faq-toggle">
+                                <i class="bi bi-chevron-down"></i>
+                                <i class="bi bi-x"></i>
+                            </button>
+                        </div>
+                        @endforeach
+
+
+                        {{-- <div class="faq">
                                 <h3 class="faq-title">
                                     How do I pay for my order?
                                 </h3>
@@ -65,20 +67,25 @@
                                     <i class="fas bi-x" aria-hidden="true"></i>
                                 </button>
                             </div> --}}
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-12 order-0 order-md-0 d-flex align-content-lg-end">
-                        <div class="image-wrapper">
-                            <img class="img-fluid"
-                                src="{{ asset('frontend/assets/images/others/undraw_Questions_re_1fy7.png') }}"
-                                alt="" srcset="">
 
-                        </div>
+                        <a href="{{ route('helpDesk') }}">
+                            <div class="faq">
+                               <h3 class="faq-title">Additional Questions</h3>
+                            </div>
+                        </a>
+
                     </div>
                 </div>
+                <div class="col-md-6 col-12 order-0 order-md-0 d-flex align-content-lg-end">
+                    <div class="image-wrapper">
+                        <img class="img-fluid" src="{{ asset('frontend/assets/images/others/undraw_Questions_re_1fy7.png') }}" alt="" srcset="">
 
+                    </div>
+                </div>
             </div>
 
         </div>
+
     </div>
+</div>
 @endsection
