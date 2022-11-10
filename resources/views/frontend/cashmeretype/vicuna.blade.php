51cd62d1448c84af05c5d74453bbbe7c3957e2dc
Kpokharel3 <krishna.pokharel@elscript.com>
2022-11-10 15:16:04 +0545

    cashmere type design started

---
+++ resources/views/frontend/cashmeretype/vicuna.blade.php
@@ -0,0 +1,46 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+
+<div class="container-fluid py-4">
+    <div class="section-padding">
+        <!-- cashmere type container -->
+        <div class="type-cashmere">
+            <div class="type-cashmere-content">
+                <!-- Cashmere type image -->
+                <div class="col-lg-4">
+                    <div class="type-image">
+
+                    </div>
+                </div>
+                <!-- cashmere detail -->
+                <div class="col-lg-8">
+                    <div class="cashmere-detail">
+                        <div class="heading">
+                            <span class="cashmere-detail-heading">
+                                Vicuna
+
+                            </span>
+                        </div>
+                        <div class="paragraph">
+                            <p>Vicuna wool or the fleece of vicuna is an ultimate luxurious fabric that is used to make stunning, durable, comfortable and timeless garments. This fiber is held as the finest natural fiber in the world and is called the fiber of the Gods. It is obtained from a rare animal called vicuna. This exceptional creature is the national animal of Peru.</p>
+                            <p>Vicuna belongs to the camel family and approximately 0.5 kilos of vicuna wool can be extracted in a year from one vicuna. It can only be sheared once in 2 year.The fiber made out of this rare animal was once earmarked solely for royalty-Incan nobility, such was the aura and fame of the Vicuna fleece. The fibers of Vicuna boast half the thickness of the finest wool in the globe. This fiber is a hair fiber and is made of vicunas’ hair. Several sumptuous as well as tremendously relaxing products of vicuna wool out there are socks, shawls, suits, coats and sweaters. In addition to these, Vicuna wool blankets and throws are a popular and splendid choice for home décor. Vicuna fabric is golden-brown in natural (undyed) stages. As a result, the Vicuna fleece is also referred as “Gold of Andes”. Vicuna fleece beats every other hair fiber and wool when it comes to fineness and splendor and hence is the costliest textile product of the world.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+@endsection
