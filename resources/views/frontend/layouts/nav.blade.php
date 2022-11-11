afe1e2df24611943e61b2b9e94d96cfba2150087
stunnerparas <prsdahal@gmail.com>
2022-11-11 07:35:59 +0545

    Code Filter

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -38,7 +38,6 @@ Backend Developer : Rudra Rajbanshi.
     </div>
     <div class="offcanvas_header_menu">
         <ul>
-            {{-- <li><a href="./index.html">Cashmere</a></li> --}}
             @foreach (getParentCategories() as $main)
                 <li class="offcanvas-li" id="offcanvas-women-li">
                     <a href="{{ route('filter', ['type' => 'category', 'slug' => $main->slug]) }}">{{ $main->name ?? '' }}
@@ -71,178 +70,6 @@ Backend Developer : Rudra Rajbanshi.
                     @endif
                 </li>
             @endforeach
-            {{-- <li class="offcanvas-li" id="offcanvas-men-li">
-                <a href="#">Men <i class="fas fa-caret-right" id="rotate-icon6"></i></a>
-                <ul class="offcanvas-submenu" id="men-submenu">
-                    <li id="submenu-menu5">
-                        <a href="#">Ready To Wear
-                            <i class="fas fa-caret-right" id="rotate-icon7"></i></a>
-                        <ul class="offcanvas-submenu-menu" id="men-clothes">
-                            <li>
-                                <a href="#" class="pane-link">Dress</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Top </a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Bottom</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Coats</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Hodiee</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Cotton Dress</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Shirt and tops</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Footwears</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li id="submenu-menu6">
-                        <a href="#">Accessories
-                            <i class="fas fa-caret-right" id="rotate-icon8"></i></a>
-                        <ul class="offcanvas-submenu-menu" id="men-accessories">
-                            <li>
-                                <a href="#" class="pane-link">Belt</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Bags </a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Gloves</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Sunglasses</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Scarves</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li id="submenu-menu7">
-                        <a href="#">Shoes <i class="fas fa-caret-right" id="rotate-icon9"></i></a>
-                        <ul class="offcanvas-submenu-menu" id="men-shoes">
-                            <li>
-                                <a href="#" class="pane-link">Goldstar</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Converse </a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Nike</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Addidas</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Reebok</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li id="accessories-submenu-li">
-                <a href="#">Accesories <i class="fas fa-caret-right" id="rotate-icon10"></i></a>
-                <ul class="offcanvas-submenu" id="accessories-submenu">
-                    <li id="submenu-menu8">
-                        <a href="#">Earrings
-                            <i class="fas fa-caret-right" id="rotate-icon11"></i></a>
-                        <ul class="offcanvas-submenu-menu" id="accessories-earring">
-                            <li>
-                                <a href="#" class="pane-link">Pandora</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Harry Winston</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Jewelry Heaven</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Sunkissed Jewelry</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Diamonds Inc</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Passion Jewellers</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Serendipity Jewelry</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li id="submenu-menu9">
-                        <a href="#">Belts <i class="fas fa-caret-right" id="rotate-icon12"></i></a>
-                        <ul class="offcanvas-submenu-menu" id="accessories-belt">
-                            <li>
-                                <a href="#" class="pane-link">Allen Solly</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Van Heusen</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Wrangler</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Lee Cooper</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Paul Smith</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li id="submenu-menu10">
-                        <a href="#">Hats <i class="fas fa-caret-right" id="rotate-icon13"></i></a>
-                        <ul class="offcanvas-submenu-menu" id="accessories-hat">
-                            <li>
-                                <a href="#" class="pane-link">Bailey</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Goorin</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">New Era</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Lock & Co</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Stetson</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li id="submenu-menu11">
-                        <a href="#">Sunglasses
-                            <i class="fas fa-caret-right" id="rotate-icon14"></i></a>
-                        <ul class="offcanvas-submenu-menu" id="accessories-sunglasses">
-                            <li>
-                                <a href="#" class="pane-link">Ray-Ban</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Oakley</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Gucci</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Prada</a>
-                            </li>
-                            <li>
-                                <a href="#" class="pane-link">Persol</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="">Home</a></li>
-            <li><a href="">Look Book</a></li>
-            <li><a href="">Our World</a></li> --}}
         </ul>
     </div>
 
@@ -266,10 +93,6 @@ Backend Developer : Rudra Rajbanshi.
                     <div class="header__top__left">
                         <span class="currency">Currency:</span>
                         <div class="header__top__hover">
-                            {{-- <span>NPR</span>
-                            <ul>
-                                <li>EUR€</li>
-                            </ul> --}}
                             <form action="{{ route('currency.setter') }}" method="POST">
                                 @csrf
                                 <select name="currency" onchange="this.form.submit()" id="">
