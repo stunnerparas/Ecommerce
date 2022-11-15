5bee43a394d2a61b77773ca5455e4f827ed3b808
Kpokharel3 <krishna.pokharel@elscript.com>
2022-11-15 13:56:57 +0545

    term and condition page creation completed

---
+++ resources/views/frontend/terms/term.blade.php
@@ -0,0 +1,68 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+<div class="custom-made-content container py-5">
+    <span class="request-heading">Terms and Contidions</span>
+
+
+
+</div>
+<div class="container list-item py-2">
+    <span class="terms-heading">Pricing</span>
+    <ul class="p-3">
+        <li>The prices will be based on the FOB or CIF.</li>
+        <li>Sampling charges will be 1.5 times on production prices.</li>
+        <li>The prices can be negotiated for larger order quantity.</li>
+        <li>The prices can be changed if the yarn prices will be increased / decreased at the time placing of production order.</li>
+        <li>The prices can be changed if the garment weight will be increased / decreased on the basis of final specs of garment.</li>
+    </ul>
+</div>
+<div class="container list-item py-4">
+    <span class="terms-heading">Sample Order</span>
+    <ul class="p-3">
+        <li>NO MOQ.</li>
+       </ul>
+</div>
+<div class="container list-item py-2">
+    <span class="terms-heading">Production Order :</span>
+    <ul class="p-3">
+        <li>MOQ : 25 Pcs per color/style, 100 Pcs per style ( 4 colors ).</li>
+       </ul>
+</div>
+<div class="container list-item py-2">
+    <span class="terms-heading">Lead Time  ( ON TIME DELIVERY )</span>
+    <ul class="p-3">
+        <li> Sample Delivery lead time - 7 to 11 days - FOR BASIC STYLES</li>
+        <li>Production Delivery lead time - 21 to 41 days for Smaller order, 41 to 61 days for Larger order.</li>
+        <li>Sample Delivery lead time - 7 to 21 days - FOR DESIGNER STYLES</li>
+       </ul>
+</div>
+<div class="container list-item py-2">
+    <span class="terms-heading">Payment</span>
+    <ul class="p-3">
+        <li>For order qty - Value below USD $ 20000.00 - 100% advance by bank transfer with confirmed order. </li>
+        <li> For order qty - Value above USD $ 20000.00 - 60% advance by bank transfer with confirmed order and 40% before shipment by bank transfer before 7 days prior shipment date.</li>
+        
+       </ul>
+</div>
+<div class="container list-item py-2">
+    <span class="terms-heading">Conditional Weight </span>
+    <ul class="p-3">
+        <li>The conditional weight of garment will be +/- 3% to 5% </li>
+       
+        
+       </ul>
+</div>
+<div class="container list-item py-2">
+    <span class="terms-heading">Yarn Quality & Workmanship Assurance</span>
+    <ul class="p-3">
+        <li>Yarn quality assurance - 100%  </li>
+        <li> Workmanship assurance - 97% and +/-3% tolerance will be acceptable.</li>
+       
+        
+       </ul>
+</div>
+
+
+
+@endsection
