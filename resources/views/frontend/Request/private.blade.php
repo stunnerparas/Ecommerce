e7bd480f1c7818b7046430f7eee66afe33c25715
Kpokharel3 <krishna.pokharel@elscript.com>
2022-11-15 13:21:14 +0545

    private label page creation completed

---
+++ resources/views/frontend/Request/private.blade.php
@@ -0,0 +1,34 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+
+<div class="colorRequest-image">
+
+    <img src="{{ asset('frontend/assets/images/privatelabelbanner.jpg') }}" class="w-100 h-100" alt="">
+
+</div>
+<!-- Content -->
+<div class="custom-made-content container py-5">
+    <span class="request-heading">Private Label</span>
+    <p class="py-3 text-justify">Kanchan Cashmere is the best and largest manufacturer of cashmere items in Nepal. We have been in the market since 2008 and we plan to offer the service of private label Cashmere.
+
+        A private label cashmere is produced by a third party as per the contract and by using the retailer brand name it can be sold in the market. Moreover, it is called ODM or white label cashmere.
+
+        A retailer can choose their own design and style with their own brand name or logo. You can specify the system of labeling, packing, and everything about the manufactured goods.
+
+        And after confirming all your choices as per your desire will be delivered to your warehouse and store.
+
+        You don’t have to worry about the concept for private labeling, we have some samples and templates that will help you to decide and structure your labeling, brand name, and logo.
+
+        We will save your time and as well as cost, you don’t have to go here and there with a lengthy process, everything is available with us which will definitely make you easy and reliable.
+    </p>
+    <span class="private-sub-heading">There are 3 steps for private label service, that you must have to know:</span>
+    <ul class="p-4"> 
+        <li>Your brand name and logo on label and tags</li>
+        <li>Packaging</li>
+        <li>Shipping your Order</li>
+    </ul>
+</div>
+
+
+@endsection
