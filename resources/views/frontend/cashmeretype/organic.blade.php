a1e4d1e91e59f06789f567b938971a32df0ead0b
Kpokharel3 <krishna.pokharel@elscript.com>
2022-11-15 15:18:49 +0545

    organic cashmere page development completed

---
+++ resources/views/frontend/cashmeretype/organic.blade.php
@@ -0,0 +1,107 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+
+<div class="container py-4">
+    <div class="section-padding">
+        <!-- cashmere type container -->
+        <div class="type-cashmere">
+            <div class="type-cashmere-content">
+                <div class="row">
+                    <!-- Cashmere type image -->
+                    <div class="col-lg-4 col-md-12  col-sm-12">
+                        <div class="type-image">
+                            <img src="{{ asset('frontend/assets/images/organic.png') }}" class="w-100 h-100" alt="">
+
+                        </div>
+                    </div>
+                    <!-- cashmere detail -->
+                    <div class="col-lg-8 col-md-12 col-sm-12">
+                        <div class="cashmere-detail my-3 ">
+                            <div class="heading">
+                                <span class="cashmere-detail-heading">
+                                    Organic Cashmere
+
+                                </span>
+                            </div>
+                            <div class="paragraph py-3">
+                                <p class="text-justify">Organic cashmere is one among the purest, smoothest and most comfortable natural fibers in the world. This simple and elegant cashmere category is obtained by combining the goats rather than shearing them. The goats that are used for producing cashmere have 2-layer coats. Among the 2 layers, the undercoat is used for making the cashmere. The undercoat layer comprises soft and thin hair.
+
+                                    This pure and pristine organic cashmere is used to make a variety of everyday products like sweaters, hats, thermal gear, blazers, scarves, shawls, poncho, gloves dresses and so on. The organic cashmere is pure and unbleached fiber. Cashmere is produced from goats. The breed of goat used to produce fine organic cashmere is found in Mongolia, Northern India, Afghanistan, Southwest China, Tibet and Iran.</p>
+                                <p class="text-justify">So, the question arises- why this fixed breed of goats?
+                                    The nomadic goats mentioned in above regions are forced to develop soft, dense and fleecy fibers underneath their rough overcoat to protect themselves from the cold. When the weather warms up and temperature rises, the goats start to lose their coats. The fur producer then combs these goats and ships the raw materials which are slightly processed to produce organic cashmere.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- Cashmere Featured -->
+                    <div class="col-lg-12 my-2">
+                        <div class="featured-box section-padding ">
+                            <span class="cashmere-detail-heading">Characteristics of Organic Cashmere</span>
+                            <ul class="py-2">
+                                <li>Minimal weights of the products</li>
+                                <li>Incredible insulation and adaptation in case of temperature fluctuations.</li>
+                                <li>Chic and versatile garments like autumn coats, shawls and so on.</li>
+                                <li>Heat retention abilities.</li>
+                                <li>Best natural fiber in the world.</li>
+                                <li>Best natural fiber in the world.</li>
+                                <li>Natural color is golden brown.</li>
+                                <li>Can be sheared only 1 time in every 2 year.</li>
+                                <li>Ultra-soft fleece extracted from an extraordinary animal called vicuna.</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <!-- cashmere quality  -->
+                    <div class="col-lg-12 my-2">
+                        <div class="featured-box">
+                            <span class="cashmere-detail-heading">Organic cashmere vs wool</span>
+                            <p class="text-justify py-2">Cashmere and wool are 2 of the softest and smoothest fabric known today. However, there is a subtle difference between them. For starters, while organic cashmere is prepared from goats, wools are prepared from sheep. Organic cashmere is more expensive than wool when compared to wool.  In the positive light, the products and garments made from organic cashmere offer better insulation and are approximately 8 times warmer than the latter. Wool has other advantages over cashmere when it comes to elasticity and durability.</p>
+
+                        </div>
+
+                    </div>
+                    <!-- cashmere protection -->
+                    <div class="col-lg-12">
+                        <div class="protection-content">
+                            <div class="featured-box">
+                                <span class="cashmere-detail-heading">How can you protect your organic cashmere outfits or products?</span>
+                                <p class="text-justify py-2">One of the first things you can do to preserve your organic cashmere product is not wash it every time you wear. You can simply hang it and let it dry by just air. You can use baby shampoo to wash your cashmere since it is not much chemically processed and won’t harm the composition of your fabric. Even when you use baby shampoo, make sure that you don’t overdo it. Don’t iron the cashmere clothes directly. </p>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- cashmere protection -->
+                    <div class="col-lg-12">
+                        <div class="protection-content">
+                            <div class="featured-box">
+                                <span class="cashmere-detail-heading">Conclusion</span>
+                                <p class="text-justify py-2">In addition to apparel, cashmere also has other extensive applications like quilts, rugs and blankets. The organic cashmere is manufactured from feel good fabrics and is timeless and sustainable as well. These elegant pieces will keep you comfortable and cool in summer and warm and cozy in winter.
+                                </p>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+@endsection
