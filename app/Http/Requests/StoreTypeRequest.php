d36f490d17c8295fe2708007b6f228edd0a89261
stunnerparas <prsdahal@gmail.com>
2023-01-20 10:25:58 +0545

    Collection

--- app/Http/Requests/StoreTypeRequest.php
+++ app/Http/Requests/StoreTypeRequest.php
@@ -24,7 +24,7 @@ class StoreTypeRequest extends FormRequest
     public function rules()
     {
         return [
-            'type' => 'required'
+            // 'type' => 'required'
         ];
     }
 }
